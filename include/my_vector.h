ff67c8c8e9cc1043f0cc09748c0c46fbddc2c02e
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-02-28 19:40:21 +0545

    feat: write functions for vector init, push, get, set, free using void** as data

--- include/my_vector.h
+++ include/my_vector.h
@@ -1,4 +1,21 @@
 #ifndef MY_VECTOR_H
 #define MY_VECTOR_H
 
+#include <stddef.h>
+
+#define VECTOR_INITIAL_CAPACITY 4
+
+typedef struct {
+    void** data;
+    size_t size;
+    size_t capacity;
+} my_vector;
+
+// core
+int my_vector_init(my_vector* v);
+int vector_push(my_vector* v, void* element);
+void* vector_get(my_vector* v, size_t index);
+int vector_set(my_vector* v, size_t index, void* element);
+void vector_free(my_vector* v);
+
 #endif  // !MY_VECTOR_H
--- src/my_vector.c
+++ src/my_vector.c
@@ -1,1 +1,51 @@
 #include "my_vector.h"
+
+#include <stdlib.h>
+
+int my_vector_init(my_vector* v) {
+    if (v == NULL) return -1;
+    v->data = malloc(VECTOR_INITIAL_CAPACITY * sizeof(void*));
+    if (v->data == NULL) return -1;
+
+    v->size = 0;
+    v->capacity = VECTOR_INITIAL_CAPACITY;
+
+    return 0;
+}
+
+int vector_push(my_vector* v, void* element) {
+    if (v == NULL) return -1;
+    if (v->size == v->capacity) {
+        size_t new_capacity = v->capacity * 2;
+        void** temp = realloc(v->data, new_capacity * sizeof(void*));
+        if (temp == NULL) return -1;
+        v->data = temp;
+        v->capacity = new_capacity;
+    }
+    v->data[v->size++] = element;
+    return 0;
+}
+
+void* vector_get(my_vector* v, size_t index) {
+    if (v == NULL) return NULL;
+
+    if (index > v->size) return NULL;
+    return v->data[index];
+}
+
+int vector_set(my_vector* v, size_t index, void* element) {
+    if (v == NULL) return -1;
+
+    if (index >= v->size) return -1;
+    v->data[index] = element;
+    return 0;
+}
+
+void vector_free(my_vector* v) {
+    if (v == NULL) return;
+
+    free(v->data);
+    v->data = NULL;
+    v->size = 0;
+    v->capacity = 0;
+}
